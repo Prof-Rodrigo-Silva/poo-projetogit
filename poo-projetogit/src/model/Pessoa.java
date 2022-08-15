29fb97ca35eb47ef6dff11e177cd3e88bcc370cb
RodrigoSilva <profrodrigorosadasilva@gmail.com>
2022-08-15 19:50:05 -0300

    Segundo commit

--- poo-projetogit/src/model/Pessoa.java
+++ poo-projetogit/src/model/Pessoa.java
@@ -4,6 +4,7 @@ public class Pessoa {
 	
 	private String nome;
 	private int idade;
+	private String endereco;
 	
 	public Pessoa() {
 		// TODO Auto-generated constructor stub
@@ -24,6 +25,15 @@ public class Pessoa {
 	public void setIdade(int idade) {
 		this.idade = idade;
 	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	
 	
 	
 
